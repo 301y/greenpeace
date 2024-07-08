8961989320b41e15809a5261a28bec268157469c
301y <301days@naver.com>
2024-07-08 16:17:15 +0900

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -63,9 +63,9 @@ $(function () {
     spaceBetween: 80,//갤러리 사이 간격
     centeredSlides: true,//센터모드
     slidesperview: true,
-    speed: 2500,//버튼을 슬라이드가 넘어가는 시간
+    speed: 800,//버튼을 슬라이드가 넘어가는 시간
     autoplay: {
-      delay: 4000,//자동으로 넘어가기 전 머무르는 시간
+      delay: 2500,//자동으로 넘어가기 전 머무르는 시간
       disableOnInteraction: false,
     },
     loop: true,//슬라이드 무한반복
@@ -99,9 +99,9 @@ $(function () {
     slidesPerView: 3.2, //보여지는 갤러리 수
     spaceBetween: 24, //갤러리 사이 간격
     centeredSlides: true, //센터모드
-    speed: 1500, //버튼을 슬라이드가 넘어가는 시간
+    speed: 800, //버튼을 슬라이드가 넘어가는 시간
     autoplay: {
-      delay: 25852500, //자동으로 넘어가기 전 머무르는 시간
+      delay: 2500, //자동으로 넘어가기 전 머무르는 시간
       disableOnInteraction: false,
     },
     loop: true, //슬라이드 무한반복
@@ -275,9 +275,9 @@ $(function () {
   var swiper = new Swiper(".bannerslide .gallery-center_inner ", {
     slidesPerView: 1.1, //보여지는 갤러리 수
     spaceBetween: 10, //갤러리 사이 간격
-    speed: 1500, //버튼을 슬라이드가 넘어가는 시간
+    speed: 800, //버튼을 슬라이드가 넘어가는 시간
     autoplay: {
-      delay: 3500, //자동으로 넘어가기 전 머무르는 시간
+      delay: 2500, //자동으로 넘어가기 전 머무르는 시간
       disableOnInteraction: false,
     },
     loop: true, //슬라이드 무한반복
@@ -309,9 +309,9 @@ $(function () {
 $(function () {
   var swiper = new Swiper('.gall2 .gallery .gallery_inner ', {
     slidesPerView: 3,//보여지는 갤러리 수
-    speed: 1500,//버튼을 슬라이드가 넘어가는 시간
+    speed: 800,//버튼을 슬라이드가 넘어가는 시간
     autoplay: {
-      delay: 3500,//자동으로 넘어가기 전 머무르는 시간
+      delay: 2500,//자동으로 넘어가기 전 머무르는 시간
       disableOnInteraction: false,
     },
     loop: true,//슬라이드 무한반복
